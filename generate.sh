008c624da9be517ed0fba8c7baa7f39552e1efa6
catcoin10 <numberfile256@gmail.com>
2019-03-09 18:00:42 -0500

    Update generate.sh
    Add kill function

--- generate.sh
+++ generate.sh
@@ -1,6 +1,11 @@
 # save HTML files to computer
+# usage:
+# $0: run normal
+# $0 killhttp: stop web server
+[ $1 = killhttp ] && kill $(cat /tmp/flash.pid) && rm /tmp/flash.pid
 cd /tmp
 git clone https://github.com/catcoin10/FlashWiki
 cd FlashWiki
 python -m SimpleHTTPServer 8888 & # allow the computer to read files locally
+echo $!>/tmp/flash.pid
 firefox localhost:8888 &
