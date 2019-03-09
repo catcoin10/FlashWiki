47c3c2cc85969e218b4718d375ace82daee0f7fe
catcoin10 <numberfile256@gmail.com>
2019-03-09 18:02:20 -0500

    Update generate.sh
    Send to /dev/null

--- generate.sh
+++ generate.sh
@@ -2,10 +2,10 @@
 # usage:
 # $0: run normal
 # $0 killhttp: stop web server
-[ $1 = killhttp ] && kill $(cat /tmp/flash.pid) && rm /tmp/flash.pid
+[ $1 = killhttp ] && kill $(cat /tmp/flash.pid) && rm /tmp/flash.pid && exit
 cd /tmp
 git clone https://github.com/catcoin10/FlashWiki
 cd FlashWiki
-python -m SimpleHTTPServer 8888 & # allow the computer to read files locally
+python -m SimpleHTTPServer 8888>/dev/null 2>&1 & # allow the computer to read files locally
 echo $!>/tmp/flash.pid
-firefox localhost:8888 &
+firefox localhost:8888 2>/dev/null 1>&2 &
