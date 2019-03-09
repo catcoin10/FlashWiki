9cc7af579b8085298ea4e24d4581c32097ca72c2
catcoin10 <numberfile256@gmail.com>
2019-03-09 18:03:27 -0500

    Update generate.sh
    Add " "

--- generate.sh
+++ generate.sh
@@ -6,6 +6,6 @@
 cd /tmp
 git clone https://github.com/catcoin10/FlashWiki
 cd FlashWiki
-python -m SimpleHTTPServer 8888>/dev/null 2>&1 & # allow the computer to read files locally
+python -m SimpleHTTPServer 8888 >/dev/null 2>&1 & # allow the computer to read files locally
 echo $!>/tmp/flash.pid
 firefox localhost:8888 2>/dev/null 1>&2 &
