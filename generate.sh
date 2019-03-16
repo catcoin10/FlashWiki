5cdbba85af028ea6b853821ac7f0c0590ac47eb7
catcoin10 <numberfile256@gmail.com>
2019-03-16 19:28:36 -0400

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,15 +1,2 @@
-# save HTML files to computer
-# usage:
-# $0: run normal
-# $0 killhttp: stop web server
-if [ $1 = killhttp ]; then
-  kill $(cat /tmp/flash.pid)
-  rm /tmp/flash.pid
-  exit 0
-fi
-cd /tmp
-git clone https://github.com/catcoin10/FlashWiki
-cd FlashWiki
-python -m SimpleHTTPServer 8888 >/dev/null 2>&1 & # allow the computer to read files locally
-echo $!>/tmp/flash.pid
-firefox localhost:8888 2>/dev/null 1>&2 &
+# The old file is bad
+# ignore
